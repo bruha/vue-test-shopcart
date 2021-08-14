ccfd51a30b733a7e924cb43f7a0bc2de6c677d07
bruha <bruha.mail@gmail.com>
2021-08-14 10:32:58 +0300

    Add Balance and Expenses

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Balance: ${{balance}}</h3>
+    <h3>Expenses:</h3>
+    <ul class="expenses-list">
+      <li v-for="expense in expenses" :key="$index">
+        <Expense :category="expense.category" :amount="expense.amount" :comment="expense.comment" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Expense from './components/Expense.vue'
+const CATEGORY = {
+  HOUSING: 'Housing',
+  FOOD: 'Food',
+  INSURANCE: 'Insurance',
+  TRANSPORTATION: 'Transportation',
+  TAXES: 'Taxes'
+}
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Expense
+  },
+  data() {
+    return {
+      balance: 1000,
+      expenses: [{
+        category: CATEGORY.HOUSING,
+        amount: 200,
+        comment: 'Electricity'
+      }, {
+        category: CATEGORY.FOOD,
+        amount: 30,
+        comment: 'Banana'
+      }, {
+        category: CATEGORY.TAXES,
+        amount: 100
+      }]
+    }
   }
 }
 </script>
@@ -21,8 +50,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h3 {
+  margin: 0;
+}
+.expenses-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Expense.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="expense">
+    {{category}}: ${{ amount }} <span v-if="comment">({{comment}})</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Expense',
+  props: {
+    amount: String,
+    category: String,
+    comment: String
+  }
+}
+</script>
+
+<style scoped>
+.expense {
+
+}
+</style>
